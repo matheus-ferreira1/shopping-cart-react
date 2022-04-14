f7b46d9c080aa4629971f4806fb87aaaf989a7d8
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-14 15:49:02 -0300

    mudança no css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,11 @@ export default function Header() {
    return (
       <header className='row background center'>
          <div>
-            <a href="#">
-               <h1>25,5 de Maio Online</h1>
-            </a>
+            <h1>25,5 de Maio Online</h1>
          </div>
          <div>
-            <a href="#">Carrinho</a>
-            <a href="#">Faça Login</a>
+            {/* <a href="#">Carrinho</a>
+            <a href="#/">Faça Login</a> */}
          </div>
       </header>
    )
