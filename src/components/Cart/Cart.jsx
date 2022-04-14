ccc09b2f6bfe90b033cf801a3a31170e9cee8a2c
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-14 15:40:31 -0300

    finalização do carrinho, alterações no css

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,24 +3,46 @@ import './Cart.scss'
 
 export default function Cart(props) {
   const { cartItems, onAdd, onRemove } = props;
+  const itemsPrice = cartItems.reduce((acc, cur) => acc + cur.price * cur.qty, 0);
+  const shippingCost = itemsPrice > 2000 ? 0 : 50;
+  const totalPrice = itemsPrice + shippingCost;
+
   return (
     <aside className='background col1'>
       <h2>Seu Carrinho</h2>
       <div>
         {cartItems.length === 0 && <div>Carrinho vazio</div>}
-        {cartItems.map((item) => (
-          <div key={item.id} className="row align-row">
-            <div className="col2">{item.name}</div>
-            <div className="col2 btn-container">
-              <button onClick={() => onRemove(item)}>-</button>
-              <button onClick={() => onAdd(item)}>+</button>
-            </div>
-            <div className="col2 text-right">
-              {item.qty} x $ {item.price.toFixed(2)}
-            </div>
-          </div>
-        ))}
       </div>
+      {cartItems.map((item) => (
+        <div key={item.id} className="row align-row">
+          <div className="col2">{item.name}</div>
+          <div className="col2 btn-container">
+            <button onClick={() => onRemove(item)}>-</button>
+            <button onClick={() => onAdd(item)}>+</button>
+          </div>
+          <div className="col2 text-right">
+            {item.qty} x $ {item.price.toFixed(2)}
+          </div>
+        </div>
+      ))}
+      {cartItems.length !== 0 && (
+        <div className='final-cart'>
+          <hr />
+          <div className="row">
+            <div className="col2">Valor do Item</div>
+            <div className="col1 text-right">$ {itemsPrice.toFixed(2)}</div>
+          </div>
+          <div className="row">
+            <div className="col2">Valor do Frete</div>
+            <div className="col1 text-right">$ {shippingCost.toFixed(2)}</div>
+          </div>
+          <div className="row">
+            <div className="col2"><strong>Valor Total da Compra</strong></div>
+            <div className="col1 text-right"><strong>$ {totalPrice.toFixed(2)}</strong></div>
+          </div>
+        </div>
+      )}
+
     </aside>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
 export default function Header() {
-  return (
-    <header className='row background center'>
-       <div>
-          <a href="">
-             <h1>25,5 de Maio Online</h1>
-          </a>
-       </div>
-       <div>
-          <a href="">Carrinho</a>
-          <a href="">Faça Login</a>
-       </div>
-    </header>
-  )
+   return (
+      <header className='row background center'>
+         <div>
+            <a href="#">
+               <h1>25,5 de Maio Online</h1>
+            </a>
+         </div>
+         <div>
+            <a href="#">Carrinho</a>
+            <a href="#">Faça Login</a>
+         </div>
+      </header>
+   )
 }
